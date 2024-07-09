2128ea91b5acf258585e1bbcc2eb28f03e91997c
CP21H <cp21h@fsu.edu>
2024-07-09 14:54:20 -0400

    Beginning of main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Filesystem.h"
 
+void init_menu();
+void help_pages();
+void cli();
+
 int main() {
     FileSystem<std::string> fs;
 
+    /*
     fs.addDirectory("root");
     fs.addDirectory("childNode1");
     fs.addDirectory("childNode2");
@@ -22,6 +27,36 @@ int main() {
     fs.listDirectories();
 
     fs.getFileData("subrootFile");
+    */
+
+    init_menu();
 
     return 0;
+}
+
+void init_menu() {
+    std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+    std::cout << "%                              %" << std::endl;
+    std::cout << "%       CLI - FILE SYSTEM      %" << std::endl;
+    std::cout << "%        Cristhian Prado       %" << std::endl;
+    std::cout << "%                              %" << std::endl;
+    std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+    std::cout << std::endl;
+}
+
+void help_pages() {
+
+}
+
+void cli() {
+    std::string clInput;
+
+    do {
+        // make a char by char parser here that takes in user input
+        // and then once it reaches the first white space, creates a command out of those chars
+
+        // if those chars make up a valid command, move to proper command logic / code
+
+        // if that command requires a directory name / file name then parse chars after white space
+    } while (clInput != "q" && clInput != "Q");
 }
